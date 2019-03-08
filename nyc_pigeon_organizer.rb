481af31df55fe1487ab7c85389095dba149399e6
Eyal Toledano <eyaltoledano@me.com>
2019-03-08 16:21:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
+def populate(item)
+  if key == item && attribute == item
+    values.each do |trait, names|
+      if names.include?(pigeon)
+        attribute_data << trait.to_s
+      end
+    end
+  end
+end
+
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_list = {}
   pigeon_data.each do |attribute, trait|
@@ -23,42 +33,31 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_list.each do |pigeon, data|
     data.each do |attribute, attribute_data|
         pigeon_data.each do |key,values|
-          if key == :color && attribute == :color
-            values.each do |trait, names|
-              if names.include?(pigeon)
-                attribute_data << trait.to_s
-              end
-            end
-          end
-          if key == :gender && attribute == :gender
-            values.each do |trait, names|
-              if names.include?(pigeon)
-                attribute_data << trait.to_s
-              end
-            end
-          end
-          if key == :lives && attribute == :lives
-            values.each do |trait, names|
-              if names.include?(pigeon)
-                attribute_data << trait.to_s
-              end
-            end
-          end
-          # These three if statements aren't DRY.
-          # How could I do something like this?
-          #
-          # def populate(item)
-          #   if key == item && attribute == item
-          #     values.each do |trait, names|
-          #       if names.include?(pigeon)
-          #         attribute_data << trait.to_s
-          #       end
+          # if key == :color && attribute == :color
+          #   values.each do |trait, names|
+          #     if names.include?(pigeon)
+          #       attribute_data << trait.to_s
           #     end
           #   end
           # end
-          # populate(:color)
-          # populate(:gender)
-          # populate(:lives)
+          # if key == :gender && attribute == :gender
+          #   values.each do |trait, names|
+          #     if names.include?(pigeon)
+          #       attribute_data << trait.to_s
+          #     end
+          #   end
+          # end
+          # if key == :lives && attribute == :lives
+          #   values.each do |trait, names|
+          #     if names.include?(pigeon)
+          #       attribute_data << trait.to_s
+          #     end
+          #   end
+          # end
+
+          populate(:color)
+          populate(:gender)
+          populate(:lives)
         end
     end
   end
